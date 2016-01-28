15390f9fd96e2fba43bd2c8764e25f9d94182e1f
Kevin Dustin <iamkevindustin@gmail.com>
2016-01-28 11:52:37 -0700

    started working on new knearest

--- dohardstuff.py
+++ dohardstuff.py
@@ -1,4 +1,5 @@
 import requests
+import math
 
 file = requests.get("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz")
 
@@ -14,6 +15,20 @@ f = open("tmp.meta.gz", "wb")
 f.write(file.content)
 f.close()
 
+file = requests.get("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz")
+
+f = open("test.gz", "wb")
+
+f.write(file.content)
+f.close()
+
+file = requests.get("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz")
+
+f = open("test.meta.gz", "wb")
+
+f.write(file.content)
+f.close()
+
 import gzip
 import struct
 
@@ -22,21 +37,16 @@ import struct
 def unt(stuff):
     return struct.unpack(">i", stuff)[0]
 
-f = gzip.open("tmp.gz", "rb")
-meta = gzip.open("tmp.meta.gz", "rb")
-print("Magic: " + str(unt(f.read(4))))
-print("Meta-magic: " + str(unt(meta.read(4))))
+def rankPoint(me, test):
+    total = 0
+    for i in range(len(me.pixels)):
+        for j in range(len(me.pixels[i])):
+            total = total + pow((me.pixels[i][j] - test.pixels[i][j]), 2)
+    return math.sqrt(total)
 
-count = unt(f.read(4))
-meta.read(4) # discard the size
 
-data = []
-
-for i in range(count):
-    rows = unt(f.read(4))
-    cols = unt(f.read(4))
-    
-    data.append(dict(
+def unpackImage(image, meta):
+    data = dict(
         number = meta.read(1),
         pixels = []#[]
     ))
@@ -44,11 +54,77 @@ for i in range(count):
     for row in range(rows):
         
         # gotta initialize the 2nd layer here
-        data[i]['pixels'].append([])
+        data['pixels'].append([])
         
-        data[i]['number'] = meta.read(1)
+        data['number'] = meta.read(1)
         
         for col in range(cols):
-            data[i]['pixels'][row].append(f.read(1))
+            data['pixels'][row].append(image.read(1))
+    
+    return Image(data["pixels"], data["number"])
 
-print(len(data[0]))
+#This is a good POPO
+class Image(object):
+    pixels = []
+    number = 0
+
+    def __init__(self, pixels, number):
+        self.pixels = pixels
+        self.number = number
+
+f = gzip.open("tmp.gz", "rb")
+meta = gzip.open("tmp.meta.gz", "rb")
+test = gzip.open("test.gz", "rb")
+testmeta = gzip.open("test.meta.gz", "rb")
+
+print("Magic: " + str(unt(f.read(4))))
+print("Meta-magic: " + str(unt(meta.read(4))))
+print("Magic Test: " + str(unt(test.read(4))))
+print("Meta-magic Test: " + str(unt(testmeta.read(4))))
+
+trainingcount = unt(f.read(4))
+meta.read(4) # discard the size
+testcount = unt(test.read(4))
+testmeta.read(4)
+
+rows = unt(f.read(4))
+cols = unt(f.read(4))
+unt(test.read(4))
+unt(test.read(4))
+
+assert rows == 28
+
+
+topten = []
+testimage = unpackImage(test, testmeta)
+for i in range(1000):
+    trained = unpackImage(f, meta)
+    rank = rankPoint(testimage, trained)
+    if len(topten) < 10:
+        topten.append(rank)
+    else:
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
